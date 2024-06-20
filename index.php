6fd7f77c9323d8a94f242654947d75bd66d3be08
danielNishimura <nishimurani@gmail.com>
2024-06-20 13:58:21 -0300

    updated index font size

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
-<?php require 'pages/header.php' ?>
-
-<link rel="stylesheet" href="assets/css/style.css">
-
-<div id="page-landing">
-    <div class="container">
-        <div class="logo-container">
-            <h1>Gera Ordem de Serviço</h1>
-            <h2>Sistema gerador de <span> Ordem de serviço</span></h2>
-        </div>
-    
-        <img class="land-image" src="./assets/images/land.jpg" alt="imagem da land page">
-    
-        <div class="buttons-container">
-            <a href="ordem.php">Acesso a página de ordem</a>
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Ordem de Serviço</title>
+</head>
+<body class="landing">
+    <div id="page-landing">
+        <div class="container">
+            <div class="logo-container">
+                <h1>Gera Ordem de Serviço</h1>
+                <h2>Sistema gerador de <span> Ordem de serviço</span></h2>
+            </div>
+        
+            <img class="land-image" src="./assets/images/land.jpg" alt="imagem da land page">
+        
+            <div class="buttons-container">
+                <a href="ordem.php">Acesso a página de ordem</a>
+            </div>
         </div>
     </div>
-</div>
+</body>
+</html>
 
-<?php require 'pages/footer.php' ?>
+
